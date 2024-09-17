54b31c301e8767a818294d28742ebb506652cc4d
gustavolhonda <gustavo.honda@estudante.ufscar.br>
2024-09-17 00:07:59 -0300

    muita coisa

---
+++ src/middleware/verifyToken.js
@@ -0,0 +1,24 @@
+// middleware/verifyToken.js
+const { admin } = require('../config/firebase');
+
+const verifyToken = async (req, res, next) => {
+  const idToken = req.cookies.access_token;
+  if (!idToken) {
+    return res.status(401).json({ error: 'Não autorizado' });
+  }
+
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    req.user = {
+      uid: decodedToken.uid,
+      email: decodedToken.email,
+      // Outros campos se necessário
+    };
+    next();
+  } catch (error) {
+    console.error('Erro ao verificar o token:', error);
+    return res.status(401).json({ error: 'Não autorizado' });
+  }
+};
+
+module.exports = verifyToken;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,23 @@
+// routes/index.js
 const express = require('express');
 const router = express.Router();
 
 const firebaseAuthController = require('../controllers/firebase-auth-controller');
+const verifyToken = require('../middleware/verifyToken');
 
+// Rotas públicas
 router.post('/api/register', firebaseAuthController.registerUser);
 router.post('/api/login', firebaseAuthController.loginUser);
 router.post('/api/logout', firebaseAuthController.logoutUser);
 router.post('/api/reset-password', firebaseAuthController.resetPassword);
+
+// Rotas protegidas
+router.get('/api/user', verifyToken, (req, res) => {
+  res.status(200).json({ user: req.user });
+});
+
+router.get('/api/protected', verifyToken, (req, res) => {
+  res.status(200).json({ message: 'Acesso concedido à rota protegida', user: req.user });
+});
 
 module.exports = router;
